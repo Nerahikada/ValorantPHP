3e58382caeac3f675b5754a94d0729d3b5b840ce
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-21 12:44:26 +0900

    Add missing strict settings

--- src/Endpoint/Enum/PlatformType.php
+++ src/Endpoint/Enum/PlatformType.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Nerahikada\ValorantPHP\Endpoint\Enum;
 
 enum PlatformType
--- src/Exception/AuthenticationFailureException.php
+++ src/Exception/AuthenticationFailureException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Nerahikada\ValorantPHP\Exception;
 
 use RuntimeException;
