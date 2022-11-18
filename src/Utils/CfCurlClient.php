103a970530039e3365d2780cf3aa32e76e09ea89
Nerahikada <hkshirorom@yahoo.co.jp>
2022-11-18 09:26:32 +0900

    fix wrong comment

--- src/Utils/CfCurlClient.php
+++ src/Utils/CfCurlClient.php
@@ -10,7 +10,7 @@ class CfCurlClient extends CurlClient
     {
         parent::__construct($enableCookies);
 
-        // Bypass cloudflare bot fight mode
+        // Bypass cloudflare bot detection (error code 1020)
         curl_setopt($this->curlHandle, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_3);
         curl_setopt($this->curlHandle, CURLOPT_SSL_CIPHER_LIST, "ECDHE+AESGCM");
         $this->setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36");
