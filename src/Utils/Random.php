1d8392691160bcebe7a1511f767e2968c94c6ab9
Nerahikada <hkshirorom@yahoo.co.jp>
2023-07-14 20:43:45 +0900

    fix style

--- src/Utils/Random.php
+++ src/Utils/Random.php
@@ -6,9 +6,9 @@ class Random
 {
     public static function string(int $length): string
     {
-        $str = '';
+        $str = "";
         for ($i = 0; $i < $length; ++$i) {
-            $str .= '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[random_int(0, 62)];
+            $str .= "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"[random_int(0, 62)];
         }
         return $str;
     }
